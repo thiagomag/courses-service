7cd655eba5709344341e747ec524b26c562a81f8
thiagomagdalena <thiagomagdalena@gmail.com>
2025-08-17 22:39:11 -0300

    minor correction

--- src/main/resources/db/migration/V1__course_db_creation.sql
+++ src/main/resources/db/migration/V1__course_db_creation.sql
@@ -2,7 +2,7 @@ CREATE TABLE courses (
     id              BIGSERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     description     TEXT,
-    category        VARCHAR(100) NOT NULL,,
+    category        VARCHAR(100) NOT NULL,
     active          BOOLEAN DEFAULT TRUE,
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by_id   BIGINT,
